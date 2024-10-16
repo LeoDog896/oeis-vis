2d841be367932a24a659a95b709008ea31b6b048
Tristan F. <LeoDog896@hotmail.com>
2024-10-16 11:39:45 -0700

    feat: add all nodes to graph

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
-use std::path::Path;
+use petgraph::Graph;
+use walkdir::WalkDir;
+use std::{fs::{self, FileType}, path::Path};
 
 use cmd_lib::run_cmd;
 use anyhow::Result;
+
+struct SequenceName(pub String);
 
 fn main() -> Result<()> {
     if !Path::new("./output").exists() {
@@ -16,7 +20,19 @@ fn main() -> Result<()> {
         )?;
     }
 
-    
+    let mut graph = Graph::<SequenceName, ()>::new();
+
+    for entry in WalkDir::new("./output/seq") {
+        let entry = entry.unwrap();
+        if entry.file_type().is_file() {
+            // great! found a node: we will first add it
+            let path = entry.into_path();
+            graph.add_node(SequenceName(path.file_stem().unwrap().to_str().unwrap().to_owned()));
+            
+            // then start connecting it!
+            let contents = fs::read(path).unwrap();
+        }
+    }
 
     Ok(())
 }
