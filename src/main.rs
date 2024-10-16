fad6e2d92848d6f56a84fa87a973f8d3974f004b
Tristan F. <LeoDog896@hotmail.com>
2024-10-16 11:30:26 -0700

    fix: use correct run_cmd syntax

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,10 @@ fn main() -> Result<()> {
         // Note: this requires that git-lfs is installed.
         // From https://stackoverflow.com/a/60729017/7589775
         run_cmd! (
-            git clone --no-checkout --depth=1 https://github.com/oeis/oeisdata.git ./output
-            git sparse-checkout init --cone
-            git sparse-checkout set seq
-            git checkout
+            git clone --no-checkout --depth=1 "https://github.com/oeis/oeisdata.git" ./output;
+            git sparse-checkout init --cone;
+            git sparse-checkout set seq;
+            git checkout;
         )?;
     }
 
