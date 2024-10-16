168a8ec2495cfb76c4482e4f450201a97f4e8b9c
Tristan F. <LeoDog896@hotmail.com>
2024-10-16 11:28:46 -0700

    refactor: move to rust
    ughh its a lot of graph data and deno doesnt have good graph libs. im not using py

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use std::path::Path;
+
+use cmd_lib::run_cmd;
+use anyhow::Result;
+
+fn main() -> Result<()> {
+    if !Path::new("./output").exists() {
+        // Note: this requires that git-lfs is installed.
+        // From https://stackoverflow.com/a/60729017/7589775
+        run_cmd! (
+            git clone --no-checkout --depth=1 https://github.com/oeis/oeisdata.git ./output
+            git sparse-checkout init --cone
+            git sparse-checkout set seq
+            git checkout
+        )?;
+    }
+
+    
+
+    Ok(())
+}
