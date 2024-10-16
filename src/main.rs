1604feb0446038ff348229079a502b1d57a49472
Tristan F. <LeoDog896@hotmail.com>
2024-10-16 12:26:22 -0700

    feat: base graph construction

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,51 @@
-use petgraph::Graph;
+use petgraph::{csr::DefaultIx, graph::NodeIndex, Graph};
+use regex::Regex;
 use walkdir::WalkDir;
-use std::{fs::{self, FileType}, path::Path};
+use std::{collections::{HashMap, HashSet}, fs::{self, FileType}, hash::Hash, path::Path};
 
 use cmd_lib::run_cmd;
 use anyhow::Result;
 
+#[derive(Hash, Clone, Eq, PartialEq)]
 struct SequenceName(pub String);
 
+/// A graph that consumes more space than a regular graph,
+/// but allows for O(1) lookups of nodes, without
+/// having to instantiate a large matrix node
+struct SpaceConsumingGraph<T> {
+    graph: Graph<T, ()>,
+    map: HashMap<T, NodeIndex<DefaultIx>>
+}
+
+impl<T: Hash + Eq + Clone> SpaceConsumingGraph<T> {
+    fn new() -> Self {
+        Self {
+            graph: Graph::new(),
+            map: HashMap::new()
+        }
+    }
+
+    fn get_node_or_add(&mut self, name: T) -> NodeIndex<DefaultIx> {
+        self.map.get(&name).copied().or_else(|| {
+            let index = self.graph.add_node(name.clone());
+            self.map.insert(name, index);
+            Some(index)
+        }).unwrap().clone()
+    }
+
+    fn add_edge(&mut self, first: T, second: T) {
+        let first_idx = self.get_node_or_add(first);
+        let second_idx = self.get_node_or_add(second);
+
+        // yes, we are allowing non-simple graphs!
+        // we have directional edges
+        self.graph.add_edge(first_idx, second_idx, ());
+    }
+}
+
 fn main() -> Result<()> {
+    let sequence_name_matching_regex = Regex::new(r"A\d{6,}").unwrap();
+
     if !Path::new("./output").exists() {
         // Note: this requires that git-lfs is installed.
         // From https://stackoverflow.com/a/60729017/7589775
@@ -20,17 +58,24 @@ fn main() -> Result<()> {
         )?;
     }
 
-    let mut graph = Graph::<SequenceName, ()>::new();
+    let mut graph = SpaceConsumingGraph::<SequenceName>::new();
 
     for entry in WalkDir::new("./output/seq") {
         let entry = entry.unwrap();
         if entry.file_type().is_file() {
             // great! found a node: we will first add it
+            // if it doesn't already exist
             let path = entry.into_path();
-            graph.add_node(SequenceName(path.file_stem().unwrap().to_str().unwrap().to_owned()));
+            let sequence_name = SequenceName(path.file_stem().unwrap().to_str().unwrap().to_owned());
             
-            // then start connecting it!
-            let contents = fs::read(path).unwrap();
+            // then start connecting it! we are looking for every single instance of A(6+ digits)
+            let contents = fs::read_to_string(path).unwrap();
+
+            for connected_sequence in sequence_name_matching_regex.captures_iter(&contents) {
+                let (sub_sequence_name, _) = connected_sequence.extract::<0>();
+
+                graph.add_edge(sequence_name.clone(), SequenceName(sub_sequence_name.to_owned()));
+            }
         }
     }
 
